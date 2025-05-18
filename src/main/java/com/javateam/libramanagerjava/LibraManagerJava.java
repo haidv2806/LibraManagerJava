5e95aed7e5ab39965a18fab50725d55a60ee4277
DESKTOP-F84FQ2V\haidv <haidv28062004@gmail.com>
2025-05-18 18:41:34 +0700

    Initial Commit

---
+++ src/main/java/com/javateam/libramanagerjava/LibraManagerJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javateam.libramanagerjava;
+
+/**
+ *
+ * @author haidv
+ */
+public class LibraManagerJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
