928aae05ef90abf56f385e018d58331ce9622a82
DESKTOP-F84FQ2V\haidv <haidv28062004@gmail.com>
2025-05-22 04:49:31 +0700

    chỉnh sửa cho class NXB

--- src/main/java/BackEnd_Hai/newpackage/Publisher.java
+++ src/main/java/BackEnd_Hai/newpackage/Publisher.java
@@ -5,7 +5,48 @@ package BackEnd_Hai.newpackage;
 //     TenNXB VARCHAR(50),
 //     DiaChi VARCHAR(100)
 // );
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class Publisher {
-    
+    int MaNXB;
+    String TenNXB;
+    String DiaChi;
+
+    public String addPublisher(int maNXB, String tenNXB, String diaChi) {
+        this.MaNXB = maNXB;
+        this.TenNXB = tenNXB;
+        this.DiaChi = diaChi;
+
+        String sql = "INSERT INTO publisher (MaNXB, TenNXB, DiaChi) VALUES (?, ?, ?)";
+
+        try (Connection conn = Database.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, MaNXB);
+            stmt.setString(2, TenNXB);
+            stmt.setString(3, DiaChi);
+            stmt.executeUpdate();
+
+            return "Thêm nhà xuất bản thành công";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public int getMaNXB(String tenNXB) {
+        String sql = "SELECT * FROM publisher WHERE TenNXB = ?";
+
+        try (Connection conn = Database.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, tenNXB);
+            return stmt.executeQuery().getInt("MaNXB");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
 }
