f80d6f5f2bc6ce2a0ea86a5338c79132984f4310
DESKTOP-F84FQ2V\haidv <haidv28062004@gmail.com>
2025-06-15 21:50:50 +0700

    last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 import javax.swing.SwingUtilities;
 
-import HanXacThucNguoiDung.LoginForm;
+import HanXacThucNguoiDung.*;
 import interfaceDung.Page1;
 
 public class Main {
         public static void main(String[] args) {
         // Hiển thị trang 1 khi chương trình khởi chạy
         SwingUtilities.invokeLater(() -> {
-            LoginForm LoginForm = new LoginForm();
-            LoginForm.setVisible(true);
+            MainForm MainForm = new MainForm();
+            MainForm.setVisible(true);
         });
     }
 }
